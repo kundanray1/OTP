f6a3b2066eab317030a13ae64fc1c458f3358439
kundanray1 <raykundan57@gmail.com>
2025-02-26 10:48:33 +0545

    ngrok added

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:'https://multi-worker.vercel.app',
+  baseURL: 'https://your-ngrok-url.ngrok.io',
+  headers: {
+    'ngrok-skip-browser-warning': 'true'
+  }
   // You can add other default settings here (e.g., headers, timeouts, etc.)
 });
 

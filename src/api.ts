eba022ab24d77f23dfa5d02653e86879c3282694
kundanray1 <raykundan57@gmail.com>
2025-03-06 13:13:08 +0545

    new api url

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://f202-2400-1a00-b050-ce62-81e1-1d4e-4f3a-c711.ngrok-free.app',
+  baseURL: 'https://1c6a-2400-1a00-b050-ce62-e930-ebde-872-1860.ngrok-free.app',
   headers: {
     'ngrok-skip-browser-warning': 'true'
   }
